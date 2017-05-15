f3adc072176426f2897337f544a090d457e985be
David Jiménez Martínez <djimenezmartinez@vistaprint.com>
2017-05-15 16:47:00 +0200

    Improved test

--- test/test_bgm.py
+++ test/test_bgm.py
@@ -8,33 +8,17 @@ from src.bgm import Application
 
 
 class BgmShould(TestCase):
+
     def test_playMusicIfEmulatorStationIsRunning(self):
-        vals = {
-            "emulationstatio": True
-        }
-
-        def processMock(procName):
-            if procName in vals:
-                return vals[procName]
-
-            return False
-
-        processService = MagicMock()
-        processService.processIsRunning = MagicMock(side_effect=processMock)
-        processService.anyProcessIsRunning = MagicMock(return_value=False)
-
-        processService.findPid = MagicMock(return_vale=-1)
-
-        os.listdir = MagicMock(return_value=['file1.ogg', 'file2.ogg', 'file3.ogg'])
-        os.path.exists = MagicMock(return_value=False)
 
         musicPlayer = MagicMock()
-        musicPlayer.isPlaying = False
-
-        os.listdir('/home/pi/RetroPie/music')
+        app = self._ScenarioMaker(musicPlayer)\
+            .theFollowingProcessesAreRunning("emulationstatio")\
+            .theFollowingSongsArePresent(['file1.ogg', 'file2.ogg', 'file3.ogg'])\
+            .build()
 
         previousState = {"emulatorIsRunning": False}
-        Application(processService, musicPlayer).executeState(previousState)
+        app.executeState(previousState)
 
         args, kwargs = musicPlayer.playSong.call_args
         self.assertTrue(args[0] in [os.path.join('/home/pi/RetroPie/music', 'file1.ogg'),
@@ -42,5 +26,41 @@ class BgmShould(TestCase):
                                     os.path.join('/home/pi/RetroPie/music', 'file3.ogg')])
         musicPlayer.playSong.assert_called_once()
 
-# if __name__ == '__main__':
-#    unittest.main()
+
+    class _ScenarioMaker():
+
+        def __init__(self, musicPlayer):
+            self.processService = MagicMock()
+            self.processService.anyProcessIsRunning = MagicMock(return_value=False)
+            self.processService.findPid = MagicMock(return_vale=-1)
+
+            self.musicPlayer = musicPlayer
+            self.musicPlayer.isPlaying = False
+            os.path.exists = MagicMock(return_value=False)
+
+        def aSongIsBeingPlayed(self):
+            self.musicPlayer.isPlaying = True
+            return self
+
+        def theFollowingProcessesAreRunning(self, *processes):
+            def processMock(procName):
+                if procName in processes:
+                    return True
+
+                return False
+
+            self.processService.processIsRunning = MagicMock(side_effect=processMock)
+            return self
+
+        def anEmulatorRunning(self):
+            self.processService.anyProcessIsRunning = MagicMock(return_value=True)
+            return self
+
+        def build(self):
+            return Application(self.processService, self.musicPlayer)
+
+        def theFollowingSongsArePresent(self, songs):
+            os.listdir = MagicMock(return_value=songs)
+            return self
+
+
